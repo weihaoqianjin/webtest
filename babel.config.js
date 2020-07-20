27e2144d9ba1b6f48c54386fad05576c85829068
huaban <2544239995@qq.com>
2020-07-20 18:14:02 +0800

    =fix

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,11 @@
 module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset'
+  ],
+  "plugins": [
+    "@babel/plugin-transform-runtime",
+    "@babel/plugin-syntax-dynamic-import",
+    "@babel/plugin-proposal-object-rest-spread",
+    "transform-vue-jsx"
   ]
 }
